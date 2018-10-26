372f6bdf779cdba7a05f6903a2bbb31765e4193d
Arup Sarkar <arup.sarkar@internal.salesforce.com>
2018-10-25 22:04:29 -0400

    Added CRUD for salesforce web app in dotnet using OAuth

---
+++ SalesforceWebApp/Controllers/ContactsController.cs
@@ -0,0 +1,253 @@
+﻿using Salesforce.Common.Models;
+using SalesforceWebApp.Models.Salesforce;
+using SalesforceWebApp.Salesforce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SalesforceWebApp.Controllers
+{
+    public class ContactsController : Controller
+    {
+
+        // Note: the SOQL Field list, and Binding Property list have subtle differences as custom properties may be mapped with the JsonProperty attribute to remove __c
+        const string _ContactsPostBinding = "Id,Salutation,FirstName,LastName,MailingStreet,MailingCity,MailingState,MailingPostalCode,MailingCountry,Phone,Email";
+        // GET: Contacts
+        public async Task<ActionResult> Index()
+        {
+            IEnumerable<Contact> selectedContacts = Enumerable.Empty<Contact>();
+            try
+            {
+                selectedContacts = await SalesforceService.MakeAuthenticatedClientRequestAsync(
+                    async (client) =>
+                    {
+                        QueryResult<Contact> contacts =
+                            await client.QueryAsync<Contact>("SELECT Id, Salutation, FirstName, LastName, MailingCity, MailingState, MailingCountry From Contact");
+                        return contacts.Records;
+                    }
+                    );
+            }
+            catch (Exception e)
+            {
+                this.ViewBag.OperationName = "query Salesforce Contacts";
+                this.ViewBag.AuthorizationUrl = SalesforceOAuthRedirectHandler.GetAuthorizationUrl(this.Request.Url.ToString());
+                this.ViewBag.ErrorMessage = e.Message;
+            }
+            if (this.ViewBag.ErrorMessage == "AuthorizationRequired")
+            {
+                return Redirect(this.ViewBag.AuthorizationUrl);
+            }
+            return View(selectedContacts);
+        }
+
+
+        public async Task<ActionResult> Details(string id)
+        {
+            IEnumerable<Contact> selectedContacts = Enumerable.Empty<Contact>();
+            try
+            {
+                selectedContacts = await SalesforceService.MakeAuthenticatedClientRequestAsync(
+                    async (client) =>
+                    {
+                        QueryResult<Contact> contacts =
+                            await client.QueryAsync<Contact>("SELECT Id, Salutation, FirstName, LastName, MailingStreet, MailingCity, MailingState, MailingPostalCode, MailingCountry, Phone, Email From Contact Where Id = '" + id + "'");
+                        return contacts.Records;
+                    }
+                    );
+            }
+            catch (Exception e)
+            {
+                this.ViewBag.OperationName = "Salesforce Contacts Details";
+                this.ViewBag.AuthorizationUrl = SalesforceOAuthRedirectHandler.GetAuthorizationUrl(this.Request.Url.ToString());
+                this.ViewBag.ErrorMessage = e.Message;
+            }
+            if (this.ViewBag.ErrorMessage == "AuthorizationRequired")
+            {
+                return Redirect(this.ViewBag.AuthorizationUrl);
+            }
+            return View(selectedContacts.FirstOrDefault());
+        }
+
+
+        public async Task<ActionResult> Edit(string id)
+        {
+            IEnumerable<Contact> selectedContacts = Enumerable.Empty<Contact>();
+            try
+            {
+                selectedContacts = await SalesforceService.MakeAuthenticatedClientRequestAsync(
+                    async (client) =>
+                    {
+                        QueryResult<Contact> contacts =
+                            await client.QueryAsync<Contact>("SELECT Id, Salutation, FirstName, LastName, MailingStreet, MailingCity, MailingState, MailingPostalCode, MailingCountry, Phone, Email From Contact Where Id= '" + id + "'");
+                        return contacts.Records;
+                    }
+                    );
+            }
+            catch (Exception e)
+            {
+                this.ViewBag.OperationName = "Edit Salesforce Contacts";
+                this.ViewBag.AuthorizationUrl = SalesforceOAuthRedirectHandler.GetAuthorizationUrl(this.Request.Url.ToString());
+                this.ViewBag.ErrorMessage = e.Message;
+            }
+            if (this.ViewBag.ErrorMessage == "AuthorizationRequired")
+            {
+                return Redirect(this.ViewBag.AuthorizationUrl);
+            }
+            return View(selectedContacts.FirstOrDefault());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = _ContactsPostBinding)] Contact contact)
+        {
+            SuccessResponse success = new SuccessResponse();
+            try
+            {
+                success = await SalesforceService.MakeAuthenticatedClientRequestAsync(
+                    async (client) =>
+                    {
+                        success = await client.UpdateAsync("Contact", contact.Id, contact);
+                        return success;
+                    }
+                    );
+            }
+            catch (Exception e)
+            {
+                this.ViewBag.OperationName = "Edit Salesforce Contact";
+                this.ViewBag.AuthorizationUrl = SalesforceOAuthRedirectHandler.GetAuthorizationUrl(this.Request.Url.ToString());
+                this.ViewBag.ErrorMessage = e.Message;
+            }
+            if (this.ViewBag.ErrorMessage == "AuthorizationRequired")
+            {
+                return Redirect(this.ViewBag.AuthorizationUrl);
+            }
+            if (success.Success == "true")
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(contact);
+            }
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = _ContactsPostBinding)] Contact contact)
+        {
+            String id = String.Empty;
+            try
+            {
+                id = await SalesforceService.MakeAuthenticatedClientRequestAsync(
+                    async (client) =>
+                    {
+                        return await client.CreateAsync("Contact", contact);
+                    }
+                    );
+            }
+            catch (Exception e)
+            {
+                this.ViewBag.OperationName = "Create Salesforce Contact";
+                this.ViewBag.AuthorizationUrl = SalesforceOAuthRedirectHandler.GetAuthorizationUrl(this.Request.Url.ToString());
+                this.ViewBag.ErrorMessage = e.Message;
+            }
+            if (this.ViewBag.ErrorMessage == "AuthorizationRequired")
+            {
+                return Redirect(this.ViewBag.AuthorizationUrl);
+            }
+            if (this.ViewBag.ErrorMessage == null)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(contact);
+            }
+        }
+
+        public async Task<ActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            IEnumerable<Contact> selectedContacts = Enumerable.Empty<Contact>();
+            try
+            {
+                selectedContacts = await SalesforceService.MakeAuthenticatedClientRequestAsync(
+                async (client) =>
+                {
+                    // Query the properties you'll display for the user to confirm they wish to delete this Contact
+                    QueryResult<Contact> contacts =
+                        await client.QueryAsync<Contact>(string.Format("SELECT Id, FirstName, LastName, MailingCity, MailingState, MailingCountry From Contact Where Id='{0}'", id));
+                    return contacts.Records;
+                }
+                );
+            }
+            catch (Exception e)
+            {
+                this.ViewBag.OperationName = "query Salesforce Contacts";
+                this.ViewBag.AuthorizationUrl = SalesforceOAuthRedirectHandler.GetAuthorizationUrl(this.Request.Url.ToString());
+                this.ViewBag.ErrorMessage = e.Message;
+            }
+            if (this.ViewBag.ErrorMessage == "AuthorizationRequired")
+            {
+                return Redirect(this.ViewBag.AuthorizationUrl);
+            }
+            if (selectedContacts.Count() == 0)
+            {
+                return View();
+            }
+            else
+            {
+                return View(selectedContacts.FirstOrDefault());
+            }
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(string id)
+        {
+            bool success = false;
+            try
+            {
+                success = await SalesforceService.MakeAuthenticatedClientRequestAsync(
+                    async (client) =>
+                    {
+                        success = await client.DeleteAsync("Contact", id);
+                        return success;
+                    }
+                    );
+            }
+            catch (Exception e)
+            {
+                this.ViewBag.OperationName = "Delete Salesforce Contacts";
+                this.ViewBag.AuthorizationUrl = SalesforceOAuthRedirectHandler.GetAuthorizationUrl(this.Request.Url.ToString());
+                this.ViewBag.ErrorMessage = e.Message;
+            }
+            if (this.ViewBag.ErrorMessage == "AuthorizationRequired")
+            {
+                return Redirect(this.ViewBag.AuthorizationUrl);
+            }
+            if (success)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ SalesforceWebApp/Models/Salesforce/Contact.cs
@@ -0,0 +1,223 @@
+﻿using Newtonsoft.Json;
+using Salesforce.Common.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesforceWebApp.Models.Salesforce
+{
+    public class Contact
+    {
+        [Key]
+        [Display(Name = "Contact ID")]
+        [Createable(false), Updateable(false)]
+        public string Id { get; set; }
+
+        [Display(Name = "Deleted")]
+        [Createable(false), Updateable(false)]
+        public bool IsDeleted { get; set; }
+
+        [Display(Name = "Master Record ID")]
+        [Createable(false), Updateable(false)]
+        public string MasterRecordId { get; set; }
+
+        [Display(Name = "Account ID")]
+        public string AccountId { get; set; }
+
+        [Display(Name = "Last Name")]
+        [StringLength(80)]
+        public string LastName { get; set; }
+
+        [Display(Name = "First Name")]
+        [StringLength(40)]
+        public string FirstName { get; set; }
+
+        public string Salutation { get; set; }
+
+        [Display(Name = "Full Name")]
+        [StringLength(121)]
+        [Createable(false), Updateable(false)]
+        public string Name { get; set; }
+
+        [Display(Name = "Other Street")]
+        public string OtherStreet { get; set; }
+
+        [Display(Name = "Other City")]
+        [StringLength(40)]
+        public string OtherCity { get; set; }
+
+        [Display(Name = "Other State/Province")]
+        [StringLength(80)]
+        public string OtherState { get; set; }
+
+        [Display(Name = "Other Zip/Postal Code")]
+        [StringLength(20)]
+        public string OtherPostalCode { get; set; }
+
+        [Display(Name = "Other Country")]
+        [StringLength(80)]
+        public string OtherCountry { get; set; }
+
+        [Display(Name = "Other Latitude")]
+        public double? OtherLatitude { get; set; }
+
+        [Display(Name = "Other Longitude")]
+        public double? OtherLongitude { get; set; }
+
+        [Display(Name = "Mailing Street")]
+        public string MailingStreet { get; set; }
+
+        [Display(Name = "Mailing City")]
+        [StringLength(40)]
+        public string MailingCity { get; set; }
+
+        [Display(Name = "Mailing State/Province")]
+        [StringLength(80)]
+        public string MailingState { get; set; }
+
+        [Display(Name = "Mailing Zip/Postal Code")]
+        [StringLength(20)]
+        public string MailingPostalCode { get; set; }
+
+        [Display(Name = "Mailing Country")]
+        [StringLength(80)]
+        public string MailingCountry { get; set; }
+
+        [Display(Name = "Mailing Latitude")]
+        public double? MailingLatitude { get; set; }
+
+        [Display(Name = "Mailing Longitude")]
+        public double? MailingLongitude { get; set; }
+
+        [Display(Name = "Business Phone")]
+        [Phone]
+        public string Phone { get; set; }
+
+        [Display(Name = "Business Fax")]
+        [Phone]
+        public string Fax { get; set; }
+
+        [Display(Name = "Mobile Phone")]
+        [Phone]
+        public string MobilePhone { get; set; }
+
+        [Display(Name = "Home Phone")]
+        [Phone]
+        public string HomePhone { get; set; }
+
+        [Display(Name = "Other Phone")]
+        [Phone]
+        public string OtherPhone { get; set; }
+
+        [Display(Name = "Asst. Phone")]
+        [Phone]
+        public string AssistantPhone { get; set; }
+
+        [Display(Name = "Reports To ID")]
+        public string ReportsToId { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [StringLength(128)]
+        public string Title { get; set; }
+
+        [StringLength(80)]
+        public string Department { get; set; }
+
+        [Display(Name = "Assistant's Name")]
+        [StringLength(40)]
+        public string AssistantName { get; set; }
+
+        [Display(Name = "Lead Source")]
+        public string LeadSource { get; set; }
+
+        public DateTimeOffset? Birthdate { get; set; }
+
+        [Display(Name = "Contact Description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Owner ID")]
+        [Updateable(false)]
+        public string OwnerId { get; set; }
+
+        [Display(Name = "Created Date")]
+        [Createable(false), Updateable(false)]
+        public DateTimeOffset CreatedDate { get; set; }
+
+        [Display(Name = "Created By ID")]
+        [Createable(false), Updateable(false)]
+        public string CreatedById { get; set; }
+
+        [Display(Name = "Last Modified Date")]
+        [Createable(false), Updateable(false)]
+        public DateTimeOffset LastModifiedDate { get; set; }
+
+        [Display(Name = "Last Modified By ID")]
+        [Createable(false), Updateable(false)]
+        public string LastModifiedById { get; set; }
+
+        [Display(Name = "System Modstamp")]
+        [Createable(false), Updateable(false)]
+        public DateTimeOffset SystemModstamp { get; set; }
+
+        [Display(Name = "Last Activity")]
+        [Createable(false), Updateable(false)]
+        public DateTimeOffset? LastActivityDate { get; set; }
+
+        [Display(Name = "Last Stay-in-Touch Request Date")]
+        [Createable(false), Updateable(false)]
+        public DateTimeOffset? LastCURequestDate { get; set; }
+
+        [Display(Name = "Last Stay-in-Touch Save Date")]
+        [Createable(false), Updateable(false)]
+        public DateTimeOffset? LastCUUpdateDate { get; set; }
+
+        [Display(Name = "Last Viewed Date")]
+        [Createable(false), Updateable(false)]
+        public DateTimeOffset? LastViewedDate { get; set; }
+
+        [Display(Name = "Last Referenced Date")]
+        [Createable(false), Updateable(false)]
+        public DateTimeOffset? LastReferencedDate { get; set; }
+
+        [Display(Name = "Email Bounced Reason")]
+        [StringLength(255)]
+        public string EmailBouncedReason { get; set; }
+
+        [Display(Name = "Email Bounced Date")]
+        public DateTimeOffset? EmailBouncedDate { get; set; }
+
+        [Display(Name = "Is Email Bounced")]
+        [Createable(false), Updateable(false)]
+        public bool IsEmailBounced { get; set; }
+
+        [Display(Name = "Photo URL")]
+        [Url]
+        [Createable(false), Updateable(false)]
+        public string PhotoUrl { get; set; }
+
+        [Display(Name = "Data.com Key")]
+        [StringLength(20)]
+        public string Jigsaw { get; set; }
+
+        [Display(Name = "Jigsaw Contact ID")]
+        [StringLength(20)]
+        [Createable(false), Updateable(false)]
+        public string JigsawContactId { get; set; }
+
+        [Display(Name = "Clean Status")]
+        public string CleanStatus { get; set; }
+
+        [Display(Name = "Level")]
+        public string Level__c { get; set; }
+
+        [Display(Name = "Languages")]
+        [StringLength(100)]
+        public string Languages__c { get; set; }
+
+    }
+}
